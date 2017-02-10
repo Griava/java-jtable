ac429188f77adad72cf044d88719c61f901e7899
Clément Alexandre <clmntlxndr@gmail.com>
2017-02-10 10:00:48 +0100

    Button

--- src/tableauswing/TableauSwing.java
+++ src/tableauswing/TableauSwing.java
@@ -5,13 +5,20 @@
  */
 package tableauswing;
 
+import java.awt.BorderLayout;
 import java.awt.Container;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+import java.awt.TextField;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
-import javax.swing.event.TableModelListener;
+import javax.swing.JTextField;
 
 /**
  *
@@ -24,37 +31,64 @@ public class TableauSwing {
      */
     public static void main(String[] args) throws FileNotFoundException, IOException, Exception {
         // TODO code application logic here
-        
-        JFrame fenetre = new JFrame();
-        
-        Container c = fenetre.getContentPane();
-        fenetre.setSize(400, 400);
- 
-        
-        fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
+               
         CatalogueDePannes sourceDonneesPannes = new CatalogueDePannes();
         
         
-        sourceDonneesPannes.loadFromJSON();    
+        JFrame fenetre = new JFrame();
+        
+        Container containerPrincipalFenetre = fenetre.getContentPane();
+        fenetre.setSize(400, 400);
+        fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        fenetre.setVisible(true);
+        
+        // Tableau à partir de la source de données de pannes :
         
         JTable table = new JTable(sourceDonneesPannes);
-
-        c.add(new JScrollPane(table));
         
-        sourceDonneesPannes.loadFromXML();
+        // On ajoute le tableau au container principal :
+        containerPrincipalFenetre.add(new JScrollPane(table));
         
-        fenetre.setVisible(true);
-       
+        // Faire un container
+        
+        Container footer = new Container();     
+        
+        // Mettre un layout au container en columns
+        
+        footer.setLayout(new GridLayout(1, 3));
+        
+        // Faire des componsants :
+        
+        JTextField idTextField = new JTextField("id");
+        JTextField nomTextField = new JTextField("nom");
+        JButton bouton = new JButton("+");
+        
+        // Mettre les champs dans le container
+        
+        footer.add(idTextField);
+        footer.add(nomTextField);
+        footer.add(bouton);
+        
+        // Ajouter le container ci-dessous :        
+        
+        containerPrincipalFenetre.add(footer, BorderLayout.SOUTH);
+ 
+        
+        
+        
         
         
          
-        sourceDonneesPannes.loadFromCode();
         
+
+        sourceDonneesPannes.loadFromJSON();   
+        sourceDonneesPannes.loadFromXML();
+        sourceDonneesPannes.loadFromCode();        
         sourceDonneesPannes.loadFromCSV();
         
 
 
     }
+
     
 }
